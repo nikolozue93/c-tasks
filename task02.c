b2baa18ed007b97b559b48a449e3a6af53e76f0f
nikolouze93 <nikaberidze27@gmail.com>
2026-08-19 17:55:57 +0400

    created dedicated function for finding executable in the path

--- task02.c
+++ task02.c
@@ -4,6 +4,41 @@
 #include <string.h>
 
 
+char *find_in_path(const char *command, const char *path){ 
+    char *path_clone = strdup(path);
+    if(path_clone == NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    char *dir = strtok(path_clone, ":");
+    char *result = NULL;
+
+    while(dir != NULL){
+        size_t total_len = strlen(dir) + 1 + strlen(command) + 1;
+
+        char *combined = malloc(total_len);
+        if(combined == NULL){
+            printf("Memory allocation failed\n");
+            return NULL;
+        }
+
+        snprintf(combined, total_len, "%s/%s", dir, command);
+
+        if(access(combined, X_OK) == 0){
+            result = combined; // saving here to free up the path_clone in the end and not directly returning
+            break;
+        }
+        free(combined);
+
+        dir = strtok(NULL, ":");
+    }
+    free(path_clone);
+
+    return result;
+}
+
+
+
 int main(int argc, char *argv[]){
     if(argc < 2){
         fprintf(stderr, "Not enough arguments\n");
@@ -24,36 +59,13 @@ int main(int argc, char *argv[]){
         char *command = argv[i];
 
         // potential improvement: extracting from full paths like /usr/bin/ls
-        char *path_clone = strdup(path); 
-        if(path_clone == NULL){
-            fprintf(stderr, "Memory allocation failed\n");
-            return 1;
+
+        char *found_path = find_in_path(command, path);
+        if(found_path != NULL){
+            printf("%s\n", found_path);
+            found = 1;
+            free(found_path);
         }
-        char *dir = strtok(path_clone, ":");
-
-
-        while(dir != NULL){
-            // /bin + 1 for /
-            size_t total_len = strlen(dir) + 1 + strlen(command) + 1;
-            char *combined = malloc(total_len);
-
-            if(combined == NULL){
-                printf("Memory allocation failed\n");
-                return 1;
-            }
-            
-            snprintf(combined, total_len, "%s/%s", dir, command);
-
-            if(access(combined, X_OK) == 0){
-                printf("%s\n", combined);
-                found = 1;
-                free(combined);
-                break;
-            }
-            free(combined);
-            dir = strtok(NULL, ":");
-        }
-        free(path_clone);
 
         if(!found){
             fprintf(stderr, "%s not found\n", command);
