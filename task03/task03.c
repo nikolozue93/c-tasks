376fdfe1fe9ea520d7f3e90542acc02d15ec3932
nikolouze93 <nikaberidze27@gmail.com>
2026-08-20 21:48:44 +0400

    first working version

--- task03/task03.c
+++ task03/task03.c
@@ -45,7 +45,7 @@ int main(){
     }
 
     //char **p = args;
-    int i = 0;
+    int i = 0; // counting arguments
     while(fgets(buffer, sizeof(buffer), fptr) != NULL){
         char *buffer_clone = strdup(buffer);
         if(buffer_clone == NULL){
@@ -63,14 +63,84 @@ int main(){
     //     printf("\n");
     // }
 
-    pid_t pid = fork();
-    if(pid == 0){
-        execvp(args[0][0], args[0]);
-        perror("exec failed"); 
-        exit(1);
-    } else if(pid > 1){
+    //pid_t pid = fork();
+    // if(pid == 0){
+    //     execvp(args[0][0], args[0]);
+    //     perror("exec failed"); 
+    //     exit(1);
+    // } else if(pid > 1){
+    //     wait(NULL);
+    // } 
+
+
+    // int fd[2];
+    // pipe(fd);
+    // pid_t pid0 = fork();
+    // if(pid0 == 0){
+    //     dup2(fd[1], 1);
+    //     close(fd[0]);
+    //     close(fd[1]);
+    //     execvp(args[0][0], args[0]);
+    // }
+
+    // pid_t pid1 = fork();
+    // if(pid1 == 0){
+    //     dup2(fd[0], 0);
+    //     close(fd[0]);
+    //     close(fd[1]);
+    //     execvp(args[1][0], args[1]);
+    // } 
+
+    // close(fd[0]); close(fd[1]);
+    // wait(NULL); wait(NULL);
+
+
+    int fd[2];
+    int in_fd = 0;
+    for(int c = 0; c < i; c++){
+        if(c < i - 1)
+            pipe(fd);
+
+        pid_t pid = my_fork();
+        if(pid == 0){
+            if(in_fd){
+                dup2(in_fd, 0); 
+                close(in_fd);
+            }
+
+            if(c < i - 1){
+                dup2(fd[1], 1);
+                close(fd[1]);
+                close(fd[0]);
+            }
+            execvp(args[c][0], args[c]);
+            
+            fprintf(stderr, "Command \"");
+            for (int w = 0; args[c][w] != NULL; w++) {
+                fprintf(stderr, "%s", args[c][w]);
+
+                if (args[c][w + 1] != NULL) {
+                    fprintf(stderr, " ");
+                }
+            }
+            fprintf(stderr, "\" failed\n");
+            exit(1);
+
+        } else if(pid > 0){
+            if(c < i - 1)
+                close(fd[1]);
+
+            if(in_fd != 0){
+                close(in_fd);
+            }
+        }
+
+        if(c < i - 1)
+            in_fd = fd[0];
+    }
+
+    for(int j = 0; j < i; j++)
         wait(NULL);
-    } 
 
     return 0;
 }
