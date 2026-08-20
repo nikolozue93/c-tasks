be48237c6fa5d7deb3d89155f168c878d71efb12
nikolouze93 <nikaberidze27@gmail.com>
2026-08-20 22:12:55 +0400

    created dedicated functions for printing command error, parsing commands and processing pipeline

--- task03/task03.c
+++ task03/task03.c
@@ -22,7 +22,7 @@ char **split(char *s, char *delim) {
     return buffer;
 }
 
-pid_t my_fork(){ // maybe need to change to pid_t instead of int
+pid_t my_fork(){ 
     const pid_t pid = fork();
 
     if(pid == -1){
@@ -32,20 +32,25 @@ pid_t my_fork(){ // maybe need to change to pid_t instead of int
     return pid;
 }
 
-int main(){
-    FILE *fptr;
-    char **args[10] = {}; // to store commands line by line
-    char buffer[SIZE]; 
+void print_command_error(char **command){
+    char **p = command;
+    fprintf(stderr, "Command \"");
+    while (*p != NULL){
+        fprintf(stderr, "%s", *p);
+        
+        if(*(p + 1) != NULL) {
+            fprintf(stderr, " ");
+        }
 
-    fptr = fopen("commands.txt", "r");
-
-    if(fptr == NULL){
-        fprintf(stderr, "Opening commands.txt failed\n");
-        return 1;
+        p++;
     }
+    fprintf(stderr, "\" failed\n");
+}
 
-    //char **p = args;
+int parse_commands(FILE *fptr, char **args[]){
     int i = 0; // counting arguments
+    char buffer[SIZE];
+
     while(fgets(buffer, sizeof(buffer), fptr) != NULL){
         char *buffer_clone = strdup(buffer);
         if(buffer_clone == NULL){
@@ -56,45 +61,10 @@ int main(){
         i++;
     }
 
-    // for(int c = 0; c < i; c++){
-    //     for(int w = 0; args[c][w] != NULL; w++){
-    //         printf("%s ", args[c][w]);
-    //     }
-    //     printf("\n");
-    // }
+    return i;
+}
 
-    //pid_t pid = fork();
-    // if(pid == 0){
-    //     execvp(args[0][0], args[0]);
-    //     perror("exec failed"); 
-    //     exit(1);
-    // } else if(pid > 1){
-    //     wait(NULL);
-    // } 
-
-
-    // int fd[2];
-    // pipe(fd);
-    // pid_t pid0 = fork();
-    // if(pid0 == 0){
-    //     dup2(fd[1], 1);
-    //     close(fd[0]);
-    //     close(fd[1]);
-    //     execvp(args[0][0], args[0]);
-    // }
-
-    // pid_t pid1 = fork();
-    // if(pid1 == 0){
-    //     dup2(fd[0], 0);
-    //     close(fd[0]);
-    //     close(fd[1]);
-    //     execvp(args[1][0], args[1]);
-    // } 
-
-    // close(fd[0]); close(fd[1]);
-    // wait(NULL); wait(NULL);
-
-
+void process_pipeline(char **args[], int i){
     int fd[2];
     int in_fd = 0;
     for(int c = 0; c < i; c++){
@@ -114,16 +84,8 @@ int main(){
                 close(fd[0]);
             }
             execvp(args[c][0], args[c]);
-            
-            fprintf(stderr, "Command \"");
-            for (int w = 0; args[c][w] != NULL; w++) {
-                fprintf(stderr, "%s", args[c][w]);
 
-                if (args[c][w + 1] != NULL) {
-                    fprintf(stderr, " ");
-                }
-            }
-            fprintf(stderr, "\" failed\n");
+            print_command_error(args[c]);
             exit(1);
 
         } else if(pid > 0){
@@ -141,6 +103,23 @@ int main(){
 
     for(int j = 0; j < i; j++)
         wait(NULL);
+}
+
+
+int main(){
+    char **args[10] = {}; // to store commands line by line
+    FILE *fptr = fopen("commands.txt", "r");
+
+    if(fptr == NULL){
+        fprintf(stderr, "Opening commands.txt failed\n");
+        return 1;
+    }
+
+    int i = parse_commands(fptr, args); // final count of arguments
+
+    if(i > 0){
+        process_pipeline(args, i);
+    }
 
     return 0;
 }
